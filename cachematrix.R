135cede3854e3c3b146a87bf60a110d69dd5527a
Wingyan Chan <c.vvingyan@gmail.com>
2019-09-03 23:57:33 +0800

    Cache matrix assignment

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,8 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <-
 }
 
 
@@ -13,3 +14,39 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+
+
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data)
+        x$setmean(m)
+        m
+}
+
+change_vector <- function(x, new_x) {
+        x$set(new_x)
+}
+
+a_vector = c(1,2,3)
+b = makeVector(a_vector)
+cachemean(b)
